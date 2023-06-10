161c8901d4634ddb66a915cacb41caf65678b3df
shinilseop <shinilseop12@gmail.com>
2023-06-11 03:58:48 +0900

    add return jwtToken

---
+++ src/main/java/com/gaebokchi/userservice/handler/OAuth2MemberSuccessHandler.java
@@ -0,0 +1,74 @@
+package com.gaebokchi.userservice.handler;
+
+import com.gaebokchi.userservice.service.UserService;
+import com.gaebokchi.userservice.utils.JwtTokenProvider;
+import com.gaebokchi.userservice.vo.Role;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URI;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@RequiredArgsConstructor
+public class OAuth2MemberSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UserService userService;
+
+    @SuppressWarnings("RedundantThrows")
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+
+        String email = oAuth2User.getAttribute("email");
+        List<Role> authorities = List.of(Role.USER);
+
+        System.out.println("OAuth2MemberSuccessHandler.onAuthenticationSuccess");
+        System.out.println("email = " + email);
+        System.out.println("authorities = " + authorities);
+
+        /* OAuth2UserService */
+        userService.saveUser(oAuth2User);
+        redirection(request, response, email, authorities);
+    }
+
+    private void redirection(HttpServletRequest request, HttpServletResponse response, String email, List<Role> authorities) throws IOException {
+        String accessToken = delegateAccessToken(email, authorities);
+        String refreshToken = delegateRefreshToken(email);
+
+        String uri = createURI(accessToken, refreshToken).toString();
+        getRedirectStrategy().sendRedirect(request, response, uri);
+    }
+
+    private String delegateAccessToken(String email, List<Role> authorities) {
+        return jwtTokenProvider.generateAccessToken(Map.of("username", email, "roles", authorities),
+                email, jwtTokenProvider.getTokenExpiration(jwtTokenProvider.getAccessTokenExpirationSeconds()));
+    }
+
+    private String delegateRefreshToken(String email) {
+        return jwtTokenProvider.generateRefreshToken(
+                email, jwtTokenProvider.getTokenExpiration(jwtTokenProvider.getRefreshTokenExpirationSeconds()));
+    }
+
+    private URI createURI(String accessToken, String refreshToken) {
+        MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
+        queryParams.add("access_token", accessToken);
+        queryParams.add("refresh_token", refreshToken);
+
+        return UriComponentsBuilder.newInstance()
+                .scheme("http").host("localhost").port(8080).path("/home").queryParams(queryParams)
+                .build().toUri();
+    }
+}
